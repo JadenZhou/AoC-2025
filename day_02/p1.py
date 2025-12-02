ceec4d92bc5dfa042e66109083d780dbdc9c1b91
Jaden Zhou <108704365+JadenZhou@users.noreply.github.com>
2025-12-02 14:55:38 -0500

    feat: day2 p1 done

--- day_02/p1.py
+++ day_02/p1.py
@@ -1,5 +1,19 @@
-with open("./sample.in") as fin:                                    
-# with open("./input.in") as fin:                                       
+# with open("./sample.in") as fin:                                    
+with open("./input.in") as fin:                                       
     ranges = [tuple(map(int, r.strip().split("-"))) for r in fin.read().strip().split(",")]
 
-print(ranges)
+sum = 0
+for lb, rb in ranges:
+    for i in range(lb, rb + 1):
+        if len(str(i)) % 2 == 1:
+            continue
+
+        mid = len(str(i)) // 2
+
+        l = int(str(i)[:mid])
+        r = int(str(i)[mid:])
+        
+        if l == r:
+            sum += i
+
+print(sum)
